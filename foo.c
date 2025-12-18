e7838854e56a7eb8d977d9d105283fab84bc142c
vicivdc <victoria.vidic@gmx.at>
2025-12-18 12:05:11 +0000

    weihnachten

--- foo.c
+++ foo.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
+
+    // Aufgabe 1: Weihnachtskekse
+    int number_biscuits = 0;
+    int diameter_biscuit = 0;
+
+    printf("number_biscuits ist :");
+    scanf("%d", &number_biscuits);
+    printf("diameter_buiscuit ist :");
+    scanf("%d", &diameter_biscuit);
+
+    printf("Für die Glasur aller %d Kekse wird Schokolade für %d cm^2 benötigt\n");
+
+
+    // Aufgabe 2: Christbaumschmuck
+    int candles = 0;
+    int balls = 0;
+
+    printf("Anzahl der candles: ");
+    scanf("%d", &candles);
+    printf("Anzahl der balls: ");
+    scanf("%d", &balls);
+
+
 }
